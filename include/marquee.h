4f149e40b1a5706556449d14fc6e5d69e2e57066
Dave <83719612+DaveJWalker@users.noreply.github.com>
2021-11-05 15:14:20 -0700

    Added ability to reverse marquee scroll direction.

--- include/marquee.h
+++ include/marquee.h
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <ledgfx.h>
+
 
 extern CRGBArray<NUM_LEDS> leds;
 
@@ -22,10 +24,14 @@ void DrawMarquee () {
     for (int i = 0; i < NUM_LEDS; i++)
       leds[i] = c.setHue(k+=8);
 
-    // Now create a counter to keep track of the initial position of the walking black
-    // LEDs within the marquee.
-    static unsigned int scroll = 0;
-    scroll++;
+    // Now create a counter to keep track of the walking black LEDs within the marquee. 
+    static unsigned int scroll = 100;   // 'Phase' of black LEDs
+    static bool       walk_fwd = true;  // Walking forward (true) or backward?
+    if (walk_fwd)  scroll++;
+    else           scroll--;
+
+    // Every 10 seconds, reverse the scroll direction
+    EVERY_N_SECONDS(10) {walk_fwd = not walk_fwd;}
 
     // Apply the walking black LEDs in the following loop.
     for (int i=scroll % WALKING_LED_SPACES; i < NUM_LEDS/(MIRRORED_STRIP+1)-1; i += WALKING_LED_SPACES) {
@@ -36,9 +42,8 @@ void DrawMarquee () {
   }
 }
 
-#include <ledgfx.h>
 
-
+// FIXME: Not really working properly... need to look into why.
 void DrawMarqueeSmooth() {
   // Only update the frame if more than MIN_FRAME milliseonds has passed
   EVERY_N_MILLISECONDS(1) { //MIN_MARQUEE_FRAME_MS) {
@@ -50,7 +55,6 @@ void DrawMarqueeSmooth() {
     CRGB c;
     for (int i = 0; i < NUM_LEDS; i++)
       leds[i] = c.setHue(k+=8);
-
 
     static float scroll = 0.0f;
     scroll += 0.3f;
